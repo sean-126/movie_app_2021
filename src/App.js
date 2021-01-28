b87d11ab6c6faa01444b5b40ee5dd6f634c02384
sean-126 <sungsihoon@gmail.com>
2021-01-28 11:19:55 +0900

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// class는 function과 유사하나 state를 갖는다.
+
+class App extends React.Component {
+  // state에는 동적으로 변하는 데이터를 넣는다.
+  state = {
+    count: 0
+  }
+  render() {
+    return <h1>I'm a class Component</h1>;
+  }
 }
 
 export default App;
